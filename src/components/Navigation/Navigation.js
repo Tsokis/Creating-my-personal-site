b0072c7d0cb269092f53074b9a788a01fe4d1f7e
Nikos <nik.ntf1@gmail.com>
2019-03-03 01:27:48 +0200

    Navigation menu component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import '../../App.css';
+
+class Navigation extends Component {
+    constructor(props){
+        super(props);
+        // State
+        this.state = {
+            isOpen: false
+        };
+        this.handleNav = this.handleNav.bind(this);
+        this.purchaseStyle = this.purchaseStyle.bind(this);
+    }
+    // navigation menu method returns a "css" object
+    navStyle = () => {
+        return {
+            display: this.state.isOpen ? 'block': 'none',
+            listStyleType: 'none',
+            backgroundColor: '#4C8CB5',
+            position: 'absolute',
+            textDecoration:'none',
+            top: '0',
+            right: '0',
+            width: '30%',
+            height: '100vh'
+        }
+    };
+    //method for handling the button responsible for the opening and closing the navigation menu
+    handleNav(){
+        return this.setState(state => ({
+            isOpen: !state.isOpen
+        }));
+    }
+
+    render() {
+        return (
+            <div style={divStyle}>
+                <button onClick={this.handleNav} style={buttonStyle}>NAV</button>
+                <nav style={this.navStyle()}>
+                    <ul className="nav-ul">
+                        <li>{this.props.home}</li>
+                        <li>{this.props.about}</li>
+                        <li>{this.props.contact}</li>
+                    </ul>
+                </nav>
+            </div>
+        );
+    }
+}
+// "css" objects
+const divStyle = {
+  position: 'relative'
+};
+const buttonStyle = {
+    zIndex:'1',
+    position: 'absolute',
+    top: '0',
+    right: '0'
+};
+
+export default Navigation;
