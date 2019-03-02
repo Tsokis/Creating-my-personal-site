261061ebbdcb19a91a493722c81cb798da03e5fa
Nikos <nik.ntf1@gmail.com>
2019-03-03 01:27:05 +0200

    Main

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Static/header';
+import Footer from './components/Static/footer';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navigation from './components/Navigation/Navigation';
+import About from './components/Pages/About';
+import Contact from './components/Pages/Contact';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Router>
+          <div>
+            <Navigation
+                home={<Link to="/">Home</Link>}
+                about={<Link to="/about">About</Link>}
+                contact={<Link to="/contact">Contact</Link>}
+            />
+            <Route exact path="/"  render={props => (
+                <React.Fragment>
+                  <div>
+                    <Header />
+                    <p><strong>Home content</strong></p>
+                  </div>
+                </React.Fragment>
+            )} />
+            <Route path="/About" component={About} />
+            <Route path="/Contact" component={Contact} />
+            <Footer />
+          </div>
+        </Router>
     );
   }
 }
---
+++ src/components/Static/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const headerTitle = () =>(
+    <header>
+        <h3>Nikos Ntafopoulos</h3>
+        <h4>Software Engineer</h4>
+    </header>
+);
+
+export default headerTitle;
