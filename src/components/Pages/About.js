d1cdb46cea2919ba90d9960a382b9a1be5b43f03
Nikos <nik.ntf1@gmail.com>
2019-03-10 22:04:33 +0200

    testing image if renders on about page

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import project from '../../../src/images/project.jpg'
 
 const About = () => {
+    console.log(project);
     return (
-        <div>
-            <p>About page </p>
-        </div>
+        <section>
+            <h3>About page</h3>
+            <img src={project} alt="testaki"/>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque exercitationem ipsam mollitia necessitatibus non obcaecati repellat reprehenderit voluptates? Inventore, iure?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos eum excepturi minus officia quasi qui recusandae ullam vel veniam voluptate!</p>
+        </section>
     )
 };
 
