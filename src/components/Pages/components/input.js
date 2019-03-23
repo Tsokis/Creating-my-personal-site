c29dcac58556d7926e4ebb8fe5135c098527b122
Nikos <nik.ntf1@gmail.com>
2019-03-23 18:15:53 +0200

    Added id props

--- src/components/Pages/components/input.js
+++ src/components/Pages/components/input.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-
-
 let input = (props) => {
     return (
         <div>
             <label>{props.label}</label>
-            <input type={props.type} name={props.name}  placeholder={props.placeholder} value={props.value} onChange={props.onChange}/>
+            <input type={props.type} name={props.name} id={props.id} placeholder={props.placeholder} value={props.value} onChange={props.onChange}/>
         </div>
     )
 };
