5ae0bbc549231a9bcc76704f180a5757db42f22f
Nikos <nik.ntf1@gmail.com>
2019-03-23 17:55:55 +0200

    Input component with Label

---
+++ src/components/Pages/components/input.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+let input = (props) => {
+    return (
+        <div>
+            <label>{props.label}</label>
+            <input type={props.type} name={props.name}  placeholder={props.placeholder} value={props.value} onChange={props.onChange}/>
+        </div>
+    )
+};
+
+export default input;
